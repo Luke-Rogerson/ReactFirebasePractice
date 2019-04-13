6b4d6aed5319413598677eb828dd0fdb5f8571ad
Luke Rogerson <lukerogerson@aol.com>
2019-04-13 22:41:50 +0100

    Connect up to Firebase db

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Admin = () => {
+import { withFirebase } from '../Firebase';
+import UserList from './UserList';
+
+const AdminPage = () => {
+  const [users, setUsers] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(
+    ({ firebase }) => {
+      setLoading(true);
+      firebase.users().on('value', snapshot => {
+        const usersObject = snapshot.val();
+        const usersList = Object.keys(usersObject).map(key => ({
+          ...usersObject[key],
+          uid: key
+        }));
+        setUsers(usersList);
+        setLoading(false);
+      });
+      return firebase.users().off();
+    },
+    [users]
+  );
+
   return (
     <div>
       <h1>Admin</h1>
+      {loading && <div>Loading...</div>}
+      <UserList users={users} />
     </div>
   );
 };
 
-export default Admin;
+export default withFirebase(AdminPage);
--- src/components/SignUp/SignUpForm.js
+++ src/components/SignUp/SignUpForm.js
@@ -22,6 +22,9 @@ const SignUpFormBase = props => {
     props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
+        return props.firebase.user(authUser.user.uid).set({ username, email });
+      })
+      .then(() => {
         setUserDetails({ ...INITIAL_STATE });
         props.history.push(ROUTES.HOME);
       })
